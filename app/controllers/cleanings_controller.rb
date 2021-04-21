85c23809ccec1c13b2318a5bbde1e4831496bb2a
Halbard <HalbardHobby@users.noreply.github.com>
2021-04-21 13:18:19 -0500

    🕵🏻‍♂️ Naive controller implementation

--- app/controllers/cleanings_controller.rb
+++ app/controllers/cleanings_controller.rb
@@ -1,5 +1,8 @@
 class CleaningsController < ApplicationController
+  before_action :set_cleaning, only: %i[ show edit update destroy ]
+  
   def index
+    @cleanings = Cleaning.all
   end
 
   def new
@@ -7,6 +10,12 @@ class CleaningsController < ApplicationController
   end
 
   def create
+    @cleaning = Cleaning.new(cleaning_params)
+
+    if @cleaning.save
+      redirect_to @cleaning, notice: "Cleaning was successfully created."
+    else
+      render :new
   end
 
   def show
@@ -16,8 +25,29 @@ class CleaningsController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @cleaning.update(cleaning_params)
+        redirect_to @cleaning, notice: "Cleaning was successfully updated."
+      else
+        render :edit
+      end
   end
 
   def destroy
+    @cleaning.destroy
+    respond_to do |format|
+      redirect_to cleanings_url, notice: "Babble was successfully destroyed."
+    end
   end
+
+  private
+
+  def set_cleaning
+    @cleaning = Cleaning.find(params[:id])
+  end
+
+  def cleaning_params
+    params.require(:cleaning).permit(:start_time, :address_id, :client_id, :employee_id, :admin_id, :approved, :price)
+  end
+
 end

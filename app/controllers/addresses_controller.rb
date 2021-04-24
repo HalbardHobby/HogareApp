a9a1aa78f3c3e32433a28976ab39f152ba554938
Halbard <HalbardHobby@users.noreply.github.com>
2021-04-23 20:47:27 -0500

    🕵🏻‍♂️ Naive controller implementation

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,11 +1,22 @@
 class AddressesController < ApplicationController
+  before_action :set_address, only: %i[show edit update destroy]
+  
   def index
+    @addresses = Address.all
   end
 
   def new
+    @address = Address.new
   end
 
   def create
+    @address = Address.new(address_params)
+
+    if @address.save
+      redirect_to @address, notice: "Address successfully added."
+    else
+      render :new
+    end
   end
 
   def show
@@ -15,8 +26,29 @@ class AddressesController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @address.update(address_params)
+        redirect_to @address, notice: "Address was successfully updated."
+      else
+        render :edit
+      end
+    end
   end
 
   def destroy
+    @address.destroy
+    respond_to do |format|
+      redirect_to addresses_url, notice: "Address was successfully destroyed."
+    end
+  end
+
+  private
+
+  def set_address
+    @address = Address.find(params[:id])
+  end
+
+  def address_params
+    params.require(:address).permit(:alias, :address, :client_id)
   end
 end

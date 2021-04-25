cab9e25316f26c60e760fa97d6079918eb5c2884
Halbard <HalbardHobby@users.noreply.github.com>
2021-04-25 13:18:23 -0500

    🎮 Added profile control actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update]
+  before_action :set_user, only: %i[show edit update activate_admin activate_client activate_employee]
 
   def index
     @users = User.all
@@ -12,6 +12,15 @@ class UsersController < ApplicationController
   end
 
   def update
+  end
+
+  def activate_admin
+  end
+
+  def activate_client
+  end
+
+  def activate_employee
   end
 
   private

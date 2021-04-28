b6700caec6871f3be811fe1b052f670326846e02
Halbard <HalbardHobby@users.noreply.github.com>
2021-04-28 13:10:49 -0500

    🛡 Fixed name collision

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   get 'static_pages/home'
   get 'static_pages/about'
   get 'static_pages/help'
@@ -12,7 +14,6 @@ Rails.application.routes.draw do
   put 'users/activate_client/:id', to: 'users#activate_client' 
   put 'users/activate_admin/:id', to: 'users#activate_admin' 
   put 'users/activate_employee/:id', to: 'users#activate_employee' 
-  devise_for :users
   
   root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
